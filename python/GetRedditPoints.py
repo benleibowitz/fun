07f50eef88cf8474830f1bb4b0b76c26095b9a2c
benleibowitz <benleibowitz2013@gmail.com>
2015-02-11 22:04:10 -0500

    script to get points from reddit

---
+++ python/GetRedditPoints.py
@@ -0,0 +1,26 @@
+'''
+Created on Feb 7, 2015
+    Get points from reddit page source code
+@author: ben leibowitz
+'''
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+info = {}
+url = 'http://www.reddit.com/user/YOUR_USER_NAME/'
+
+hdr = {'User-Agent': 'Mozilla/5.0'}
+req = Request(url, headers=hdr)
+response = urlopen(req).read().decode('utf-8')
+
+soup = BeautifulSoup(response)
+
+commentlines = soup.find_all("p", {"class":"tagline"})
+
+for tagline in commentlines:
+    points = tagline.find("span", {"class":"score likes"})
+    if points is not None:
+        points = int(''.join(points.contents).split(' ')[0])
+        info['comment points'] = (points if not 'comment points' in info.keys() else info['comment points'] + points)
+
+print(info)
