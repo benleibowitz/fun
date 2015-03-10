c1c01193fb98ee96a4014a4a9c7825a51729d48e
benleibowitz <benleibowitz@users.noreply.github.com>
2015-03-10 16:09:05 -0400

    Create CheckingAcct.java

---
+++ java/CheckingAcct.java
@@ -0,0 +1,32 @@
+/*
+ * CheckingAcct class, which inherits from abstract class Acct
+*/
+
+public class CheckingAcct extends Acct {
+	CheckingAcct() {
+		initChecking();
+		setCurrency = false;
+		canUse = false;
+	}
+	
+	CheckingAcct(Curr curr) throws AccountNotInitializedException {
+		initChecking();
+		
+		if(curr != null) {
+			this.curr = curr;
+			setCurrency = true;
+			canUse = true;
+		} else
+			throw new AccountNotInitializedException("Cannot set null currency");
+	}
+	
+	private void initChecking() {
+		setMonthlyContributionLim();
+	}
+	
+	@Override
+	public void setMonthlyContributionLim() {
+		//for checking accounts, let's set a limit of 20000
+		monthlyContributionLim = 20000;
+	}
+}
