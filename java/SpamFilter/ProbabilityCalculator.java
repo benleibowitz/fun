d009e9b0206391b073dc1a18da7b04a428020827
benleibowitz <benleibowitz@users.noreply.github.com>
2015-03-16 16:52:26 -0400

    Update ProbabilityCalculator.java

--- java/SpamFilter/ProbabilityCalculator.java
+++ java/SpamFilter/ProbabilityCalculator.java
@@ -6,24 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProbabilityCalculator {
+	private static final String WORDBANK_FILE = "C:/Users/Ben/workspace/JavaProjects/src/spam/WordMap.csv";
+	
 	//All words in map are lowercase.
 	//Probability map contains: <word, { P(word is in spam message), P(word is in real message) }>
 	private Map<String, double[]> probabilityMap;
-	
-	private static final String WORDBANK_FILE = "C:/Users/Ben/workspace/JavaProjects/src/spam/WordMap.csv";
-	
-	//Probability that an average message is spam (stats say it could be anywhere from 0.5 to 0.8)
-	private static final double PROBABILITY_SPAM_MESSAGE = 0.5;
-	
-	/*
-	 * If a word's probability of being a spam word is more than LEGITIMATE_WORD_THRESHOLD
-	 * away from 0.5, (ie, how far it is from 50/50), use the word in calculation.
-	 * This helps us avoid words that are 50%/50% spam/real, and Bayesian poisoning
-	 * EXAMPLE: if LEGITIMATE_WORD_THRESHOLD=0.2, word will be
-	 * used if probability spam word < .3 or > .7
-	*/
-	private static final double LEGITIMATE_WORD_THRESHOLD = 0.35;
-	
 	private SpamAlgorithm algorithm;
 	
 	public ProbabilityCalculator(SpamAlgorithm algorithm) {
