33d07c8a663f749b9a6243cc6a9651a4b1b26cd7
benleibowitz <benleibowitz@users.noreply.github.com>
2015-03-18 12:02:49 -0400

    Update ProbabilityMap.java

--- java/SpamFilter/ProbabilityMap.java
+++ java/SpamFilter/ProbabilityMap.java
@@ -1,8 +1,11 @@
 /*
- * ProbabilityMap is a marker interface, used to signal
- * that subclasses are mappings for their algorithms
+ * ScoringSystem is a marker interface, used to signal
+ * that subclasses are scoring systems for their algorithms.
+ * For example, you can implement ScoringSystem and
+ * define spam / real probabilty mappings for words in an email,
+ * or certain ways of scoring words in emails
  */
 package spam;
 
-public interface ProbabilityMap {
+public interface ScoringSystem {
 }
