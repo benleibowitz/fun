739f5e47d9afdb32c090fe89728b38b83bf76108
benleibowitz <benleibowitz@users.noreply.github.com>
2015-03-18 11:28:50 -0400

    Update TestAlgorithm.java

--- java/SpamFilter/TestAlgorithm.java
+++ java/SpamFilter/TestAlgorithm.java
@@ -3,9 +3,7 @@ import java.util.Map;
 
 class TestAlgorithm implements SpamAlgorithm {
 	@Override
-	public boolean isSpam(Message message, Map<String, double[]> bodyProbabilityMap,
-			Map<String, double[]> senderProbabilityMap,
-			Map<String, double[]> subjectProbabilityMap) {
+	public boolean isSpam(Message message, ProbabilityMap probabilityMap) {
 		System.out.println("Test algorithm called on message: " + message.getBody());
 		return true;
 	}
