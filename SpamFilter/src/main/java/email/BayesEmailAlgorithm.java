757ca9abfbb7c923a6c7d61cdcaee7ed0d150f84
benleibowitz <benleibowitz@users.noreply.github.com>
2015-03-20 16:48:52 -0400

    Update and rename NaiveBayesEmailAlgorithm.java to BayesEmailAlgorithm.java

--- SpamFilter/src/main/java/email/BayesEmailAlgorithm.java
+++ SpamFilter/src/main/java/email/BayesEmailAlgorithm.java
@@ -21,14 +21,14 @@ package email;
 import java.util.Map;
 import java.util.List;
 
-public class NaiveBayesEmailAlgorithm implements SpamAlgorithm {
+public class BayesEmailAlgorithm implements SpamAlgorithm {
 	private static final double BODY_WEIGHT = 0.35;
 	private static final double SENDER_WEIGHT = 0.2;
 	private static final double SUBJECT_WEIGHT = 0.45;
 	
 	private BayesEmailScoringSystem scoringSystem;
 	
-	public NaiveBayesEmailAlgorithm(BayesEmailScoringSystem scoringSystem) {
+	public BayesEmailAlgorithm(BayesEmailScoringSystem scoringSystem) {
 		if(scoringSystem == null)
 			throw new IllegalArgumentException("Scoring system cannot be null");
 			
