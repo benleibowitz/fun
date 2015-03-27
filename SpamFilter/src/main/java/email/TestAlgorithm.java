f8dee5d8054aee3f3cc7dc8c1af72bd6791fe6e9
benleibowitz <benleibowitz@users.noreply.github.com>
2015-03-27 13:44:57 -0400

    Delete TestAlgorithm.java

--- SpamFilter/src/main/java/email/TestAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package email;
-
-class TestAlgorithm implements SpamAlgorithm {
-	@Override
-	public boolean isSpam(Email email) {
-		System.out.println("Test algorithm called on message: " + email.getBody());
-		return true;
-	}
-}
