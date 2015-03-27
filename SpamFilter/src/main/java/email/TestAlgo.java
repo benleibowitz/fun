937d29cbd8d72c9c21be59497357d0a0c6e0b14f
benleibowitz <benleibowitz@users.noreply.github.com>
2015-03-27 13:45:15 -0400

    Update TestAlgo.java

--- SpamFilter/src/main/java/email/TestAlgo.java
+++ SpamFilter/src/main/java/email/TestAlgo.java
@@ -6,7 +6,7 @@ public class TestAlgo implements SpamAlgorithm {
 	public boolean isSpam(Email email) {
 		
 		//I call this one, AssumptionAlgorithm :)
-		System.out.println("Running test algorithm");
+		System.out.println("Running test algorithm on email from sender:" + email.getSender());
 		return true;
 	}
 }
