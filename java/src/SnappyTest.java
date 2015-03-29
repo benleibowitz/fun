69d24218d7d15449100a64e703b9265a944dac16
benleibowitz <benleibowitz2013@gmail.com>
2015-03-29 10:03:39 -0400

    Add Google Snappy test

---
+++ java/src/SnappyTest.java
@@ -0,0 +1,47 @@
+/*
+ * Testing Google's Snappy compression algorithm
+ * by compressing a test CSV file.
+ */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.xerial.snappy.Snappy;
+
+public class SnappyTest {
+    public static void main(String[] args) {
+        
+        FileOutputStream oStream = null;
+        
+        try {
+            
+            byte[] fileBytes = Files.readAllBytes(Paths.get("/Users/ben/Documents/Code/CSV/testcsv.csv"));
+            String fileString = new String(fileBytes, StandardCharsets.UTF_8);
+            
+            byte[] compressedBytes = Snappy.compress(fileString);
+            
+            oStream = new FileOutputStream(new File("/Users/ben/Documents/Code/CSV/testcsv_encoded.txt"));
+            oStream.write(compressedBytes);
+
+            System.out.println("Compressed file written");
+        
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(oStream != null) {
+                
+                try {
+                    oStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                
+            }
+        }
+        
+    }
+}
