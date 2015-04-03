c4dcdde0df7a1be5a76a80b1eba2cee7b7b5dfef
benleibowitz <benleibowitz@users.noreply.github.com>
2015-04-03 15:20:17 -0400

    Create BinaryTree.java

---
+++ java/src/BinaryTree.java
@@ -0,0 +1,88 @@
+package com;
+
+import java.math.BigDecimal;
+
+public class BinaryTree {
+	private class Node {
+		//LeftChildNode will be <= current node,
+		//and rightChildNode will be > current Node
+		Node leftChildNode;
+		Node rightChildNode;
+		BigDecimal data;
+		
+		Node(BigDecimal data) {
+			this.data = data;
+		}
+		
+		public void setLeftChildNode(Node newNode) {
+			leftChildNode = newNode;
+		}
+		
+		public void setRightChildNode(Node newNode) {
+			rightChildNode = newNode;
+		}
+		
+		public Node getLeftChildNode() {
+			return leftChildNode;
+		}
+
+		public Node getRightChildNode() {
+			return rightChildNode;
+		}
+
+		private BigDecimal getData() {
+			return data;
+		}
+	}
+	
+	Node headNode;
+	int depth;
+	
+	public BinaryTree() {
+		depth = 0;
+	}
+	
+	public void add(BigDecimal data) {
+		if(headNode == null) {
+			headNode = new Node(data);
+			depth++;
+		} else {
+			int currentDepth = 1;
+			Node currentNode = headNode;
+			
+			boolean placedNode = false;
+			while(!placedNode) {
+				currentDepth++;
+
+				if(data.compareTo(currentNode.getData()) == 1) {
+					//New node is bigger than currentNode, set on right side if
+					//right side is null. else, traverse the right child node
+					if(currentNode.getRightChildNode() == null) {
+						currentNode.setRightChildNode(new Node(data));
+						placedNode = true;
+					} else {
+						currentNode = currentNode.getRightChildNode();
+					}
+				} else {
+					//New node is <= currentNode, set on left side 
+					if(currentNode.getLeftChildNode() == null) {
+						currentNode.setLeftChildNode(new Node(data));
+						placedNode = true;
+					} else {
+						currentNode = currentNode.getLeftChildNode();
+					}
+					
+				}
+			}
+
+			if(currentDepth > depth)
+				depth = currentDepth;
+		}
+
+	}
+	
+	public int getDepth() {
+		return depth;
+	}
+	
+}
