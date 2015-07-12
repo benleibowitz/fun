c2e65bb6fe8050b4f6f392c7a852cc62d22e4383
benleibowitz <benleibowitz@users.noreply.github.com>
2015-07-12 09:56:21 -0400

    Update MyStack.java

--- java/src/MyStack.java
+++ java/src/MyStack.java
@@ -1,60 +1,64 @@
 package com;
 
 
-public class MyStack {
-	private class Node {
-		private Node nextNode;
-		private Object data;
-		
-		Node(Object data) {
-			nextNode = null;
-			this.data = data;
-		}
-		
-		private Node getNextNode() {
-			return nextNode;
-		}
-		
-		private void setNextNode(Node nextNode) {
-			this.nextNode = nextNode;
-		}
-		
-		private Object getData() {
-			return data;
-		}
-	}
-	
-	private Node firstNode;
-	private int size;
-	
-	public MyStack() {
-	}
-	
-	public void add(Object obj) {
-		Node currentFirst = firstNode;
-		firstNode = new Node(obj);
-		
-		if(firstNode != null) {
-			firstNode.setNextNode(currentFirst);
-		}
-		
-		size++;
-	}
-	
-	public Object pop() {
-		if(size == 0)
-			throw new ArrayIndexOutOfBoundsException("Cannot pop from empty stack");
-		
-		Node oldFirstNode = firstNode;
-		firstNode = firstNode.getNextNode();
-		
-		size--;
-		return oldFirstNode.getData();
-	}
-	
-	public int size() {
-		return size;
-	}
+public class MyStack<E> {
+    private class Node {
+        private Node nextNode;
+        private E data;
+        
+        Node(E data) {
+            nextNode = null;
+            this.data = data;
+        }
+        
+        private Node getNextNode() {
+            return nextNode;
+        }
+        
+        private void setNextNode(Node nextNode) {
+            this.nextNode = nextNode;
+        }
+        
+        private E getData() {
+            return data;
+        }
+    }
+    
+    private Node firstNode;
+    private int size;
+    
+    public MyStack() {
+    }
+    
+    public void add(E obj) {
+        Node currentFirst = firstNode;
+        firstNode = new Node(obj);
+        
+        if(firstNode != null) {
+            firstNode.setNextNode(currentFirst);
+        }
+        
+        size++;
+    }
+    
+    public E pop() {
+        if(size == 0)
+            throw new ArrayIndexOutOfBoundsException("Cannot pop from empty stack");
+        
+        Node oldFirstNode = firstNode;
+        firstNode = firstNode.getNextNode();
+        
+        size--;
+        return oldFirstNode.getData();
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    public boolean isEmpty() {
+        return (size == 0);
+    }
 
-	
+    
 }
