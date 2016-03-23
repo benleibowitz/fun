9329972ac35972eaacc4c1c2e366712d64dcf4f3
benleibowitz <benleibowitz2013@gmail.com>
2016-03-23 18:35:45 -0400

    erlang merge sort

---
+++ erlang/algo.erl
@@ -0,0 +1,41 @@
+-module(algo).
+-export([merge_sort/1, split/1]).
+
+merge_sort(InList) when length(InList) =< 1 ->
+    InList;
+merge_sort(InList) ->
+    {L, R} = split(InList, []),
+    LSplit = merge_sort(L),
+    RSplit = merge_sort(R),
+    merge(LSplit, RSplit, []).
+
+% Merge 2 sorted arrays    
+merge([], [], Acc) ->
+    Acc;
+merge(L1, [], Acc) ->
+    [H|T] = L1,
+    merge(T, [], Acc ++ [H]);
+merge([], L2, Acc) ->
+    [H|T] = L2,
+    merge([], T, Acc ++ [H]);
+merge(L1, L2, Acc) ->
+    [H1|T1] = L1,
+    [H2|T2] = L2,
+    case H1 < H2 of
+        true ->
+            merge(T1, L2, Acc ++ [H1]);
+        false ->
+            merge(L1, T2, Acc ++ [H2])
+    end.
+
+% Splits array into 2 subarrays.
+% If array has odd length, left subarray will have 1 more
+% item than right subarray.
+split(InArr) ->
+    split(InArr, []).
+
+split(InArr, LArr)  when length(InArr) > length(LArr) ->
+    [H|T] = InArr,
+    split(T, LArr ++ [H]);
+split(InArr, LArr) ->
+    {LArr, InArr}.
