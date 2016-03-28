3249a87400ae8c97160d5b9d6dbbb9b7ceaa5946
benleibowitz <benleibowitz2013@gmail.com>
2016-03-28 12:09:29 -0400

    modify merge sort for tail recursion

--- erlang/algo.erl
+++ erlang/algo.erl
@@ -9,23 +9,24 @@ merge_sort(InList) ->
     RSplit = merge_sort(R),
     merge(LSplit, RSplit, []).
 
-% Merge 2 sorted arrays    
+% Merge 2 sorted arrays
 merge([], [], Acc) ->
-    Acc;
+    lists:reverse(Acc);
 merge(L1, [], Acc) ->
     [H|T] = L1,
-    merge(T, [], Acc ++ [H]);
+    merge(T, [], [H|Acc]);
 merge([], L2, Acc) ->
     [H|T] = L2,
-    merge([], T, Acc ++ [H]);
+    merge([], T, [H|Acc]);
 merge(L1, L2, Acc) ->
     [H1|T1] = L1,
     [H2|T2] = L2,
+
     case H1 < H2 of
         true ->
-            merge(T1, L2, Acc ++ [H1]);
+            merge(T1, L2, [H1|Acc]);
         false ->
-            merge(L1, T2, Acc ++ [H2])
+            merge(L1, T2, [H2|Acc])
     end.
 
 % Splits array into 2 subarrays.
@@ -36,6 +37,6 @@ split(InArr) ->
 
 split(InArr, LArr)  when length(InArr) > length(LArr) ->
     [H|T] = InArr,
-    split(T, LArr ++ [H]);
+    split(T, [H|LArr]);
 split(InArr, LArr) ->
-    {LArr, InArr}.
+    {lists:reverse(LArr), InArr}.
