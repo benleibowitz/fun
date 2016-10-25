77ca9491a6516f4aba737994dbef13da8b3b5eb1
benleibowitz <benleibowitz2013@gmail.com>
2016-10-25 17:53:11 -0400

    Counting sort

---
+++ java/src/main/java/com/ben/algofun/Countingsort.java
@@ -0,0 +1,53 @@
+package com.ben.algofun;
+
+/**
+ * Created by ben on 10/24/16.
+ */
+public class Countingsort {
+    /**
+     * Will find minimum and maximum element of array, then
+     * perform counting sort
+     * @param input
+     * @return sorted array
+     */
+    public static int[] sort(int[] input) {
+        if(input.length < 2) {
+            return input;
+        }
+
+        Integer max = input[0];
+        Integer min = input[0];
+        for(int item : input) {
+            if(item > max) {
+                max = item;
+            } else if(item < min) {
+                min = item;
+            }
+        }
+
+        return sort(input, min, max);
+    }
+
+    /**
+     * Sort array using the given range min and max
+     * @param input array
+     * @param min minimum element in array
+     * @param max maximum element in array
+     * @return sorted array
+     */
+    public static int[] sort(int[] input, int min, int max) {
+        int[] counts = new int[max - min + 1];
+        for(int i = 0; i < input.length; i++) {
+            counts[input[i] - min]++;
+        }
+
+        int[] result = new int[input.length];
+        int resultIdx = 0;
+        for(int i = 0; i < counts.length; i++) {
+            for(int j = 0; j < counts[i]; j++) {
+                result[resultIdx++] = i + min;
+            }
+        }
+        return result;
+    }
+}
---
+++ java/src/test/java/com/ben/algofun/CountingsortTest.java
@@ -0,0 +1,42 @@
+package com.ben.algofun;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by ben on 10/25/16.
+ */
+public class CountingsortTest {
+    @Test
+    public void testSortEmptyArray() {
+        int[] x = {};
+        int[] y = Arrays.copyOf(x, x.length);
+        assertArrayEquals(y, Countingsort.sort(x));
+    }
+
+    @Test
+    public void testSortLen1Array() {
+        int[] x = {4};
+        int[] y = Arrays.copyOf(x, x.length);
+        assertArrayEquals(y, Countingsort.sort(x));
+    }
+
+    @Test
+    public void testSort() {
+        int[] x = {2,4,6,2,3,3,2,3,5,2,3,4,3,2,2,2,3,4,1,2,3};
+        int[] y = Arrays.copyOf(x, x.length);
+        Arrays.sort(y);
+        assertArrayEquals(y, Countingsort.sort(x));
+    }
+
+    @Test
+    public void testSortWithParams() {
+        int[] x = {2,4,6,2,3,3,2,3,5,2,3,4,3,2,2,2,3,4,1,2,3};
+        int[] y = Arrays.copyOf(x, x.length);
+        Arrays.sort(y);
+        assertArrayEquals(y, Countingsort.sort(x, 1, 6));
+    }
+}
